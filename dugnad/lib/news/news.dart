cad7d5ae54cc3b3762798da1ccadf6e63ba8ab8f
somasponga <sasmob-fch-17@inclouded.hu>
2021-03-18 23:10:33 +0100

    WIP

--- dugnad/lib/news/news.dart
+++ dugnad/lib/news/news.dart
@@ -1,23 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:dugnad/news/newsRow.dart';
 
-class News extends StatelessWidget {
+class News extends StatefulWidget {
+  News({Key key}) : super(key: key);
+
+  @override
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State<News> with SingleTickerProviderStateMixin {
+  List<Tab> _tabList = [
+    Tab(
+      child: Text("Általános"),
+    ),
+    Tab(
+      child: Text("Közösségi"),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: _tabList.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  TabController _tabController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hirök"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            NewsRow(),
-            NewsRow(),
-            NewsRow(),
-          ],
-          mainAxisAlignment: MainAxisAlignment.start,
+        appBar: AppBar(
+          title: Text("Hirök"),
+          centerTitle: true,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(30.0),
+            child: TabBar(
+              indicatorColor: Colors.green[900],
+              isScrollable: false,
+              controller: _tabController,
+              tabs: _tabList,
+            ),
+          ),
         ),
-      ),
-    );
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+                child: Row(
+                  children: [
+                    NewsRow(),
+                    NewsRow(),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(),
+            ),
+          ],
+        ));
   }
 }
