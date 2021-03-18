da21f2f27d43c4d607467320fbae676c691a9b3e
somasponga <sasmob-fch-17@inclouded.hu>
2021-03-19 00:38:12 +0100

    WIP

--- dugnad/lib/news/news.dart
+++ dugnad/lib/news/news.dart
@@ -1,5 +1,6 @@
+import 'package:dugnad/news/listitem.dart';
 import 'package:flutter/material.dart';
-import 'package:dugnad/news/newsRow.dart';
+import 'package:dugnad/news/listCard.dart';
 
 class News extends StatefulWidget {
   News({Key key}) : super(key: key);
@@ -15,6 +16,45 @@ class _NewsState extends State<News> with SingleTickerProviderStateMixin {
     ),
     Tab(
       child: Text("Közösségi"),
+    ),
+  ];
+
+  List<ListItem> listTiles = [
+    ListItem(
+      "Kovács János",
+      "Jövőhéten szombaton tartunk egy baráti találkozót. Az ha túl hangosak lennénk kérem szóljanak! ",
+      "Baráti összejövetel szombaton",
+      "2021.03.19",
+    ),
+    ListItem(
+      "Nagy József",
+      "Felújítjuk a fürdőszobát, így szerda délelőtt lehet hangosak leszünk. Pár óra alatt megvagyunk. Köszi a megértést. ",
+      "Felújitás szerdán",
+      "2021.03.10",
+    ),
+    ListItem(
+      "Kovács János",
+      "Vasárnap megyek pestre. 2 ülés szabad. Sajnos eléggé meg vagyunk pakolva, max 1 hátizsák/fő fér el. ",
+      "Fuvar Pestre",
+      "2021.03.14",
+    ),
+    ListItem(
+      "Loremion Ipsonát",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis lacus odio, vel sodales sapien. ",
+      "Fuvar Pestre",
+      "2020.04.20",
+    ),
+    ListItem(
+      "Kossuth Lajos",
+      "Ha valaki arra jár, vehetne nekem egy kis regimentet. ",
+      "Üzenet",
+      "1848.??.??",
+    ),
+    ListItem(
+      "Thanos Marvel",
+      "Tegnap egy szakállas fazon világító mellkassal lerántottam a kesztyűmet és csettintgetve elszaladt. Ha valaki látta kérem értesítsen. ",
+      "Bosszút állnék",
+      "2019.04.25",
     ),
   ];
 
@@ -52,18 +92,26 @@ class _NewsState extends State<News> with SingleTickerProviderStateMixin {
           children: [
             Padding(
               padding: EdgeInsets.all(8.0),
-              child: Container(
-                child: Row(
-                  children: [
-                    NewsRow(),
-                    NewsRow(),
-                  ],
-                ),
-              ),
+              child: Container(child: ListView()),
             ),
             Padding(
               padding: EdgeInsets.all(8.0),
-              child: Container(),
+              child: Container(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  children: List.generate(6, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        color: Colors.green,
+                        child: Center(
+                          child: listcard(listTiles[index]),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
             ),
           ],
         ));
