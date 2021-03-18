d88e05c1a562831f8e7df2755b2168fddefc7505
somasponga <sasmob-fch-17@inclouded.hu>
2021-03-18 19:06:28 +0100

    WIP

--- dugnad/lib/news/news.dart
+++ dugnad/lib/news/news.dart
@@ -2,31 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:dugnad/news/newsRow.dart';
 
 class News extends StatelessWidget {
-  const News({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    final title = 'Hírek';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [NewsRow(), NewsRow(), NewsRow()],
+          mainAxisAlignment: MainAxisAlignment.start,
         ),
-        body: Center(
-          child: Column(
-            children: [newsRow, newsRow, newsRow],
-            mainAxisAlignment: MainAxisAlignment.start,
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
--- dugnad/lib/news/newsRow.dart
+++ dugnad/lib/news/newsRow.dart
@@ -1,44 +1,48 @@
 import 'package:flutter/material.dart';
 
-Widget newsRow = Container(
-  padding: const EdgeInsets.only(bottom: 5, top: 5),
-  child: Expanded(
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          padding: const EdgeInsets.only(bottom: 1),
-          child: Expanded(
-            child: Card(
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      color: Colors.amber,
-                      child: Text(
-                        'I dont have a \nclue what im1',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
+class NewsRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Expanded(
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
                   ),
-                  Expanded(
-                    flex: 7,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'I dont have a \nclue what im2',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                  elevation: 5,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: Container(
+                          child: ListTile(
+                            leading: Icon(Icons.album),
+                            title: Text('The Enchanted Nightingale'),
+                            subtitle: Text(
+                                'Music by Julie Gable. Lyrics by Sidney Stein.'),
+                          ),
+                        ),
                       ),
-                    ),
+                      Expanded(
+                        flex: 6,
+                        child: Container(color: Colors.blue),
+                      )
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
-      ],
-    ),
-  ),
-);
+      ),
+    );
+  }
+}
