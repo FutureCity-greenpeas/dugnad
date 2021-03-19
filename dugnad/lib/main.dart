2f6245c647c757a7f590a1939edb2579708bd646
andrasdenes <andresch.denes@gmail.com>
2021-03-19 03:38:25 +0100

    add logo and webview plugin

--- dugnad/lib/main.dart
+++ dugnad/lib/main.dart
@@ -44,15 +44,27 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Padding(
-          padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-          child: Text(
-            widget.title,
-            style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.w300,
-                fontFamily: "Alegreya"),
-          ),
+        title: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: CircleAvatar(
+                radius: 20.0,
+                backgroundImage: AssetImage('assets/images/lakokor.png'),
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+              child: Text(
+                widget.title,
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w300,
+                    fontFamily: "Alegreya"),
+              ),
+            ),
+          ],
         ),
       ),
       body: HomeScreen(),
