3b3b550f7310c441aa01ad16a78a8441e96f0e7c
andrasdenes <andresch.denes@gmail.com>
2021-03-19 04:27:54 +0100

    list init

--- dugnad/lib/flyer/Flyer.dart
+++ dugnad/lib/flyer/Flyer.dart
@@ -9,6 +9,32 @@ class _FlyerState extends State<Flyer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: CircleAvatar(
+                radius: 20.0,
+                backgroundImage: AssetImage('assets/images/lakokor.png'),
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+              child: Text(
+                "Szórólapok",
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w300,
+                    fontFamily: "Alegreya"),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
